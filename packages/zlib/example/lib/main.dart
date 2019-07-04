8b3fa78cb716699fcebd992404faec21681a3c9d
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-07-04 10:09:49 +0700

    add `example` into `zlib`

---
+++ packages/zlib/example/lib/main.dart
@@ -0,0 +1,51 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:zlib/zlib.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _zlibV = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String zlibV = CString.fromUtf8(zlibVersion());
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _zlibV = zlibV;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on: $_zlibV\n'),
+        ),
+      ),
+    );
+  }
+}
