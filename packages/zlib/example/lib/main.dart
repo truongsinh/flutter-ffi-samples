9bce6d904e32acd6905f08ad65129b004a632218
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-07-04 10:14:50 +0700

    working prototype with `zlib` Android

--- packages/zlib/example/lib/main.dart
+++ packages/zlib/example/lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String zlibV = CString.fromUtf8(zlibVersion());
+    String zlibV = zlibVersion();
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
--- packages/zlib/lib/zlib.dart
+++ packages/zlib/lib/zlib.dart
@@ -1,7 +1,3 @@
 library zlib;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export "./src/zlib.dart";
