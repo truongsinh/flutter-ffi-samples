1be05e44e360bab0b32e290dd9d678eedf4a8553
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-08-28 07:22:19 +0700

    Update SwiftSimplePlugin.swift

--- packages/simple/ios/Classes/SwiftSimplePlugin.swift
+++ packages/simple/ios/Classes/SwiftSimplePlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-@_cdecl("nativeSum") // export to C as `sayHello`
+@_cdecl("nativeSum") // export to C as `nativeSum`
 public func nativeSum(i1: Int, i2: Int) -> Int {    
     return i1 + i2
 }
